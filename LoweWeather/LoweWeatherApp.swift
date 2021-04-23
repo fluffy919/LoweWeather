4b975f259d938598785d862dcf03c66226f6ee2c
mobileANDweb <mobiledeveloper112075@gmail.com>
2021-04-22 23:34:00 -0400

    Initial Commit

---
+++ LoweWeather/LoweWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoweWeatherApp.swift
+//  LoweWeather
+//
+//  Created by B on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LoweWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
